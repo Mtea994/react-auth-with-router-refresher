b30811175f551e4970604bea88f48bf1362ce70a
imtinan khurshid <engg_imtinan@yahoo.com>
2024-10-02 23:46:46 +0500

    added authentication action

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,43 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request, params }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  const form = await request.formData();
+  const data = Object.fromEntries(form);
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "unsupported mode." }, { status: 422 });
+  }
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.status === 422 || response.status === 400) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json(
+      { message: "could not authenticate user" },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return redirect("/");
+}
